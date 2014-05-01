ed2c2c43160cad35a4c0627a4ce16ba7638c709e
Wesley Yao <Wes@Wesleys-MacBook-Air.local>
2014-04-30 17:14:05 -0700

    Initial Deployment

--- scraper.js
+++ scraper.js
@@ -1,8 +1,32 @@
-function getBracket()
+var url = "http://www.nbaplayoffsbracket.com/2014/index.php";
+
+function getBracket(results)
 {
-	return $("#playoffbracket").html();
+	var top = results.split('<div id="playoffbracket" class="max">')[1];
+	var bot = top.split('<footer>');
+	return bot[0];
 }
 
-var bracket = getBracket();
-//console.log( bracket );
-$("body").append(bracket);
+function changeImgTags()
+{
+	var baseURL = "http://www.nbaplayoffsbracket.com";
+	$( "img" ).each(function() {
+  		var newURL = $(this).attr("src").replace("..", baseURL);
+  		$(this).attr( "src", newURL );
+
+  		$(this).parent().css( "cursor", "default");
+	});
+}
+
+function getPage()
+{
+	$.get( url, function(results){
+		var bracket = getBracket( results );
+		$("div.bracket").append( bracket );
+		changeImgTags();
+	});
+}
+
+$( document ).ready( function(){ 
+	getPage();
+})
